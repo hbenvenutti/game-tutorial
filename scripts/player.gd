3a6b07ccacd4d1f60e057fbc2b2c58354f0b322a
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-09-08 18:14:58 -0300

    Commit inicial.

---
+++ scripts/player.gd
@@ -0,0 +1,6 @@
+extends KinematicBody2D
+
+# *** ---- Variables ------------------------------------------------------------------------ *** //
+const acceleration = 25;
+const maxSpeed = 80;
+const friction = 80;
